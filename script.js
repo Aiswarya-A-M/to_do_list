c6d27ab1d87f0aea591ce1c50c52aa7b81125a69
AiswaryaAM <aiswarya.am@neoito.com>
2023-11-03 15:01:45 +0530

    style of table changed

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-
-let array=[];/*[{
-    id:1,
-    name:"aiswarya",
-    priority:"Low",
-    date:"31/10/2023",
-    createDate:"31/2/2001"
-},{
-    id:2,
-    name:"aiswarya2",
-    priority:"High",
-    date:"31/10/2024",
-    createDate:"31/6/2001"
-}]*/
+let array=[];
 function clearTask(){
     array=[];
     display();
@@ -26,9 +13,6 @@ function view(count){
             document.getElementById("viewId").value=task.id;
         }
     })
-    /*ok.addEventListener("click",function(){
-        console.log("hit...",index);
-    });*/
     cancelViewPopup.addEventListener("click",function(){
         popup.style.display="none";
     });
@@ -84,7 +68,6 @@ function addItem(){
     }
     console.log("outside loop",count);
     const name=document.getElementById("taskName");
-    //const Id=document.getElementById("taskId");
     newTask={
             id:count+1,
             name:name.value
@@ -108,14 +91,9 @@ function check(count){
        }
 }
 function display(){
-    var taskList = document.getElementById("tableRow");
+    var taskList=document.getElementById("displayTable")
     taskList.innerHTML=" ";
     array.forEach(function (task){
-        /*const tableHead=document.createElement("tr");
-        const tableId=document.createElement("th");
-        tableId.innerHTML-"Id";
-        const tableTask=document.createElement("th");
-        tableTask.innerHTML="Task";*/
         const tableRow=document.createElement("tr");
         const tableDataId=document.createElement("td");
         const tableDataName=document.createElement("td");
@@ -137,10 +115,6 @@ function display(){
         deleteButton.textContent="Delete";
         tableDataId.innerHTML=(task.id);
         tableDataName.innerHTML=(task.name);
-
-       /* tableHead.appendChild(tableId);
-        tableHead.appendChild(tableTask);
-        taskList.appendChild(tableHead);*/
         checkDiv.appendChild(checkBox);
         buttons.appendChild(viewButton);
         buttons.appendChild(editButton);
@@ -149,110 +123,8 @@ function display(){
         tableRow.appendChild(tableDataId);
         tableRow.appendChild(tableDataName);
         tableRow.appendChild(buttons);
-        taskList.appendChild(tableRow);   
+        taskList.appendChild(tableRow);  
 });
 
 
 }
-
-
-/*let count=1;
-const array=[{
-    id:1,
-    name:"aiswarya",
-    priority:"Low",
-    date:"31/10/2023",
-    createDate:"31/2/2001"
-},{
-    id:2,
-    name:"aiswarya2",
-    priority:"High",
-    date:"31/10/2024",
-    createDate:"31/6/2001"
-}]
-function view(count){
-    const popup = document.querySelector(`.viewPopUp`)
-    popup.style.display="block";
-    array.forEach(task=>{
-        if(task.id===count){ 
-            document.getElementById("viewTask").value=task.name;
-            document.getElementById("viewId").value=task.id;
-        }
-    })
-    /*ok.addEventListener("click",function(){
-        console.log("hit...",index);
-    });
-    cancelViewPopup.addEventListener("click",function(){
-        popup.style.display="none";
-    });
-}
-function edit(count){
-    const edit = document.querySelector(`#editForm`)
-    edit.style.display="block";
-    array.forEach(task=>{
-        if(task.id===count){
-            document.getElementById("editName").value=task.name;
-            editTask.addEventListener("click",function(){
-                task.name=document.getElementById("editName").value;
-                edit.style.display="none";
-                hlo (task);
-            });
-        }    
-    }) 
-    cancelEdit.addEventListener("click",function(){
-        edit.style.display="none";
-    })
-}
-function deleteTask(count){
-    console.log("hi",count);
-    array.forEach(task=>{
-        if(task.id===count){ 
-            console.log(array.indexOf(task))
-        }
-    });
-}
-
-function addTask(){
-    document.getElementById("addForm").style.display="block";
-}
-function addItem(){
-    let count=array[array.length-1].id;
-    const name=document.getElementById("taskName");
-    //const Id=document.getElementById("taskId");
-    newTask={
-            id:count+1,
-            name:name.value
-    }
-    array.push(newTask);
-    var taskList = document.getElementById("displayTable");
-    taskList.innerHTML=" ";
-    array.forEach(function (task){
-        const tableRow=document.createElement("tr");
-        const tableDataId=document.createElement("td");
-        const tableDataName=document.createElement("td");
-        const buttons=document.createElement("td");
-        const viewButton=document.createElement("button");
-        const editButton=document.createElement("button");
-        const deleteButton=document.createElement("button");
-        viewButton.setAttribute("onclick", `view(${task.id})`);
-        editButton.setAttribute("onclick", `edit(${task.id})`);
-        editButton.setAttribute("onclick", `deleteTask(${task.id})`);
-        viewButton.textContent="View";
-        editButton.textContent="Edit";
-        deleteButton.textContent="Delete";
-        tableDataId.innerHTML=(task.id);
-        tableDataName.innerHTML=(task.name);
-        buttons.appendChild(viewButton);
-        buttons.appendChild(editButton);
-        buttons.appendChild(deleteButton);
-        tableRow.appendChild(tableDataId);
-        tableRow.appendChild(tableDataName);
-        tableRow.appendChild(buttons);
-        taskList.appendChild(tableRow);
-        
-    });
-    cancelItem();
-}
-function cancelItem(){
-    document.getElementById("addForm").style.display="none";
-}*/
