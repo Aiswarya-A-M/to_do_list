19259ee7163232437207aad8c42c7b2a7bb36c30
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-13 10:41:12 +0530

    check box refreshing problem

--- script.js
+++ script.js
@@ -1,5 +1,217 @@
+// function clearTask() {
+//   clearPopup.style.display = "block";
+// }
+
+// function clearConfirmation() {
+//   tasks = [];
+//   localStorage.setItem("tasks", JSON.stringify(tasks));
+//   display();
+//   clearSuccess.style.display = "block";
+//   setTimeout(function () {
+//     clearSuccess.style.display = "none";
+//     clearPopup.style.display = "none";
+//   }, 2000);
+// }
+
+// function view(id) {
+//   const popup = document.querySelector(`#viewPopUp`);
+//   popup.style.display = "block";
+//   const taskToView = tasks.find((obj) => obj.id === id);
+//   document.getElementById("viewTask").textContent = taskToView.name;
+//   document.getElementById("viewPriority").textContent = taskToView.priority;
+//   document.getElementById("viewTime").textContent = taskToView.scheduledTime;
+//   const formattedCreateDateScheduledDate = new Date(
+//     taskToView.scheduledDate
+//   ).toLocaleDateString("en-GB");
+//   const formattedCreateDate = new Date(
+//     taskToView.createDate
+//   ).toLocaleDateString("en-GB");
+//   document.getElementById("viewDate").textContent =
+//     formattedCreateDateScheduledDate;
+//   document.getElementById("viewCreateDate").textContent = formattedCreateDate;
+//   document.getElementById("viewStatus").textContent = taskToView.status;
+//   document.getElementById("viewId").textContent = taskToView.id;
+//   cancelView.addEventListener("click", function () {
+//     popup.style.display = "none";
+//   });
+// }
+
+// function edit(id) {
+//   const edit = document.querySelector(`#editForm`);
+//   edit.style.display = "block";
+//   const taskToEdit = tasks.find((task) => task.id === id);
+//   document.getElementById("editName").value = taskToEdit.name;
+//   document.getElementById("editPriority").value = taskToEdit.priority;
+//   document.getElementById("editDate").value = taskToEdit.scheduledDate;
+//   document.getElementById("editTime").value = taskToEdit.scheduledTime;
+//   editTask.addEventListener("click", function () {
+//     const updatedName = document.getElementById("editName").value;
+//     const updatedPriority = document.getElementById("editPriority").value;
+//     const updatedDate = document.getElementById("editDate").value;
+//     const updatedTime = document.getElementById("editTime").value;
+//     const updatedTasks = tasks.map((task) => {
+//       if (task.id === id) {
+//         return {
+//           id: task.id,
+//           name: updatedName,
+//           priority: updatedPriority,
+//           scheduledDate: updatedDate,
+//           scheduledTime: updatedTime,
+//           createDate: task.createDate,
+//           status: task.status,
+//         };
+//       }
+//       return task;
+//     });
+//     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+//     display();
+//     edit.style.display = "none";
+//     editSuccess.style.display = "block";
+//     setTimeout(function () {
+//       editSuccess.style.display = "none";
+//     }, 2000);
+//   });
+// }
+
+// function deleteTask(id) {
+//   deletePopup.style.display = "block";
+//   tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+//   dltconfirmation.addEventListener("click", function () {
+//     const index = tasks.findIndex((obj) => obj.id === id);
+//     if (index !== -1) {
+//         tasks.splice(index, 1);
+//         localStorage.setItem("tasks", JSON.stringify(tasks));
+//         deleteSuccess.style.display = "block";
+//         display();
+//     }
+//   });
+//   setTimeout(function () {
+//     deleteSuccess.style.display = "none";
+//     deletePopup.style.display = "none";
+//   }, 2000);
+// }
+
+// function check(id) {
+//   let element = document.getElementById("toggle" + id);
+//   tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+//   const taskForCheck = tasks.find((obj) => obj.id === id);
+//   if (element.style.textDecoration != "line-through") {
+//     element.style.textDecoration = "line-through";
+//     taskForCheck.status = "Completed";
+//   } else {
+//     element.style.textDecoration = "none";
+//     taskForCheck.status = "not completed";
+//   }
+//   localStorage.setItem("tasks", JSON.stringify(tasks));
+// }
+
+// function cancelItem() {
+//   addForm.style.display = "none";
+//   clearPopup.style.display = "none";
+//   deletePopup.style.display = "none";
+//   document.getElementById("editForm").style.display = "none";
+// }
+
+// function addTask() {
+//   document.getElementById("addForm").style.display = "block";
+// }
+
+// function addItem() {
+//   let name = document.getElementById("taskName");
+//   let priority = document.getElementById("taskPriority");
+//   let scheduledTime = document.getElementById("taskTime");
+//   let scheduledDate = document.getElementById("taskDate").value;
+//   if(name!=="" && priority!=="" && scheduledTime!=="" && scheduledDate!==""){
+//     let id;
+//     if (tasks.length === 0) {
+//       id = 1;
+//     } else {
+//       id = Math.max(...tasks.map((task) => task.id)) + 1;
+//     }
+//     let createDate = new Date();
+//     let status = "not completed";
+//     tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+//     newTask = {
+//       id: id,
+//       name: name.value,
+//       priority: priority.value,
+//       scheduledTime: scheduledTime.value,
+//       scheduledDate: scheduledDate,
+//       createDate: createDate,
+//       status: status,
+//     };
+//     tasks.push(newTask);
+//     tasks.sort((a, b) => new Date(b.createDate) - new Date(a.createDate));
+//     localStorage.setItem("tasks", JSON.stringify(tasks));
+//     addSuccess.style.display = "block";
+//     setTimeout(function () {
+//       addSuccess.style.display = "none";
+//     }, 2000);
+//     display();
+//     cancelItem();
+//     addForm.reset();
+//   }
+// }
+
+// function display() {
+//   var taskList = document.getElementById("tableBody");
+//   tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+//   taskList.innerHTML = " ";
+//   tasks.forEach(function (task) {
+//     const tableRow = document.createElement("tr");
+//     switch (task.priority) {
+//       case "low":
+//         tableRow.style.backgroundColor = "rgb(222, 229, 222)";
+//         break;
+//       case "medium":
+//         tableRow.style.backgroundColor = "rgb(68, 200, 68)";
+//         break;
+//       case "high":
+//         tableRow.style.backgroundColor = "rgb(229, 92, 28)";
+//         break;
+//       case "severe":
+//         tableRow.style.backgroundColor = "rgb(221, 33, 67)";
+//         break;
+//       default:
+//         tableRow.style.backgroundColor = "white";
+//     }
+//     const coloumnForButtons = document.createElement("td");
+//     const tableDataId = document.createElement("td");
+//     const tableDataName = document.createElement("td");
+//     tableDataName.id = "taskList";
+//     tableDataName.id = "toggle" + `${task.id}`;
+//     const checkBox = document.createElement("input");
+//     const checkDiv = document.createElement("td");
+//     checkDiv.className = "checkTable";
+//     coloumnForButtons.className = "coloumnForButtons";
+//     const viewButton = document.createElement("button");
+//     const editButton = document.createElement("button");
+//     const deleteButton = document.createElement("button");
+//     checkBox.type = "checkbox";
+//     viewButton.setAttribute("onclick", `view(${task.id})`);
+//     editButton.setAttribute("onclick", `edit(${task.id})`);
+//     deleteButton.setAttribute("onclick", `deleteTask(${task.id})`);
+//     checkBox.setAttribute("onclick", `check(${task.id})`);
+//     viewButton.textContent = "View";
+//     editButton.textContent = "Edit";
+//     deleteButton.textContent = "Delete";
+//     tableDataId.innerHTML = task.id;
+//     tableDataName.innerHTML = task.name;
+//     checkDiv.appendChild(checkBox);
+//     coloumnForButtons.appendChild(viewButton);
+//     coloumnForButtons.appendChild(editButton);
+//     coloumnForButtons.appendChild(deleteButton);
+//     tableRow.appendChild(checkDiv);
+//     tableRow.appendChild(tableDataId);
+//     tableRow.appendChild(tableDataName);
+//     tableRow.appendChild(coloumnForButtons);
+//     taskList.appendChild(tableRow);
+//   });
+// }
+
+// window.addEventListener("load", display());
 function clearTask() {
-  clearPopup.style.display = "block";
+  clearPopUp.style.display = "block";
 }
 
 function clearConfirmation() {
@@ -9,13 +221,13 @@ function clearConfirmation() {
   clearSuccess.style.display = "block";
   setTimeout(function () {
     clearSuccess.style.display = "none";
-    clearPopup.style.display = "none";
+    clearPopUp.style.display = "none";
   }, 2000);
 }
 
 function view(id) {
-  const popup = document.querySelector(`#viewPopUp`);
-  popup.style.display = "block";
+  const popUp = document.querySelector(`#viewPopUp`);
+  popUp.style.display = "block";
   const taskToView = tasks.find((obj) => obj.id === id);
   document.getElementById("viewTask").textContent = taskToView.name;
   document.getElementById("viewPriority").textContent = taskToView.priority;
@@ -32,7 +244,7 @@ function view(id) {
   document.getElementById("viewStatus").textContent = taskToView.status;
   document.getElementById("viewId").textContent = taskToView.id;
   cancelView.addEventListener("click", function () {
-    popup.style.display = "none";
+    popUp.style.display = "none";
   });
 }
 
@@ -74,42 +286,46 @@ function edit(id) {
 }
 
 function deleteTask(id) {
-  deletePopup.style.display = "block";
+  deletePopUp.style.display = "block";
   tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   dltconfirmation.addEventListener("click", function () {
     const index = tasks.findIndex((obj) => obj.id === id);
     if (index !== -1) {
-        tasks.splice(index, 1);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-        deleteSuccess.style.display = "block";
-        display();
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      deleteSuccess.style.display = "block";
+      display();
     }
   });
   setTimeout(function () {
     deleteSuccess.style.display = "none";
-    deletePopup.style.display = "none";
+    deletePopUp.style.display = "none";
   }, 2000);
 }
 
 function check(id) {
-  let element = document.getElementById("toggle" + id);
-  tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  console.log("at check")
+  //let element = document.getElementById("toggle" + id);
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  console.log(tasks);
   const taskForCheck = tasks.find((obj) => obj.id === id);
-  if (element.style.textDecoration != "line-through") {
-    element.style.textDecoration = "line-through";
+  if (taskForCheck.status === "not completed") {
     taskForCheck.status = "Completed";
+    console.log(taskForCheck.status)
   } else {
-    element.style.textDecoration = "none";
     taskForCheck.status = "not completed";
+    console.log(taskForCheck.status)
   }
   localStorage.setItem("tasks", JSON.stringify(tasks));
+  console.log("end")
+  display();
 }
 
 function cancelItem() {
   addForm.style.display = "none";
-  clearPopup.style.display = "none";
-  deletePopup.style.display = "none";
-  document.getElementById("editForm").style.display = "none";
+  clearPopUp.style.display = "none";
+  deletePopUp.style.display = "none";
+  editForm.style.display = "none";   //3ei3
 }
 
 function addTask() {
@@ -117,25 +333,25 @@ function addTask() {
 }
 
 function addItem() {
-  let name = document.getElementById("taskName");
-  let priority = document.getElementById("taskPriority");
-  let scheduledTime = document.getElementById("taskTime");
+  let name = document.getElementById("taskName").value;
+  let priority = document.getElementById("taskPriority").value;
+  let scheduledTime = document.getElementById("taskTime").value;
   let scheduledDate = document.getElementById("taskDate").value;
-  if(name!=="" && priority!=="" && scheduledTime!=="" && scheduledDate!==""){
-    let id;
-    if (tasks.length === 0) {
-      id = 1;
-    } else {
-      id = Math.max(...tasks.map((task) => task.id)) + 1;
-    }
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  if (
+    name &&
+    priority &&
+    scheduledTime &&
+    scheduledDate 
+  ) {
+    let id = tasks.length + 1;
     let createDate = new Date();
     let status = "not completed";
-    tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    newTask = {
-      id: id,
-      name: name.value,
-      priority: priority.value,
-      scheduledTime: scheduledTime.value,
+    let newTask = {
+      id,
+      name: name,
+      priority: priority,
+      scheduledTime: scheduledTime,
       scheduledDate: scheduledDate,
       createDate: createDate,
       status: status,
@@ -150,15 +366,27 @@ function addItem() {
     display();
     cancelItem();
     addForm.reset();
+  }else{
+    alert("all fields are required")
   }
 }
 
 function display() {
-  var taskList = document.getElementById("tableBody");
+  let taskList = document.getElementById("tableBody");
   tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   taskList.innerHTML = " ";
   tasks.forEach(function (task) {
     const tableRow = document.createElement("tr");
+    const tableDataName = document.createElement("td");
+    const checkBox = document.createElement("input");
+    const viewButton = document.createElement("button");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    const checkDiv = document.createElement("td");
+    const columnForButtons = document.createElement("td");
+    const tableDataId = document.createElement("td");
+    checkBox.type = "checkbox";
+    checkDiv.className = "checkTable";
     switch (task.priority) {
       case "low":
         tableRow.style.backgroundColor = "rgb(222, 229, 222)";
@@ -175,19 +403,16 @@ function display() {
       default:
         tableRow.style.backgroundColor = "white";
     }
-    const coloumnForButtons = document.createElement("td");
-    const tableDataId = document.createElement("td");
-    const tableDataName = document.createElement("td");
+    if (task.status === "Completed"){
+      console.log("no way")
+      tableDataName.style.textDecoration = "line-through";
+      checkBox.checked;
+    }else{
+      tableDataName.style.textDecoration = "none";
+    } 
     tableDataName.id = "taskList";
     tableDataName.id = "toggle" + `${task.id}`;
-    const checkBox = document.createElement("input");
-    const checkDiv = document.createElement("td");
-    checkDiv.className = "checkTable";
-    coloumnForButtons.className = "coloumnForButtons";
-    const viewButton = document.createElement("button");
-    const editButton = document.createElement("button");
-    const deleteButton = document.createElement("button");
-    checkBox.type = "checkbox";
+    columnForButtons.className = "columnForButtons";
     viewButton.setAttribute("onclick", `view(${task.id})`);
     editButton.setAttribute("onclick", `edit(${task.id})`);
     deleteButton.setAttribute("onclick", `deleteTask(${task.id})`);
@@ -198,13 +423,13 @@ function display() {
     tableDataId.innerHTML = task.id;
     tableDataName.innerHTML = task.name;
     checkDiv.appendChild(checkBox);
-    coloumnForButtons.appendChild(viewButton);
-    coloumnForButtons.appendChild(editButton);
-    coloumnForButtons.appendChild(deleteButton);
+    columnForButtons.appendChild(viewButton);
+    columnForButtons.appendChild(editButton);
+    columnForButtons.appendChild(deleteButton);
     tableRow.appendChild(checkDiv);
     tableRow.appendChild(tableDataId);
     tableRow.appendChild(tableDataName);
-    tableRow.appendChild(coloumnForButtons);
+    tableRow.appendChild(columnForButtons);
     taskList.appendChild(tableRow);
   });
 }
